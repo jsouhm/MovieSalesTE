f411fa6bdafdce75270121627983e042b9e36e7f
jsouhm <tinaj.zhou@gmail.com>
2026-05-05 19:05:06 -0400

    ICA 12 Complete

--- MovieSalesT/Form1.Designer.cs
+++ MovieSalesT/Form1.Designer.cs
@@ -36,21 +36,23 @@
             btnAddMovie = new Button();
             cboMovieGenre = new ComboBox();
             lstMovieOutput = new ListBox();
+            btnClear = new Button();
+            btnQuit = new Button();
             ((System.ComponentModel.ISupportInitialize)numMoviePrice).BeginInit();
             SuspendLayout();
             // 
             // lblMovieTitle
             // 
             lblMovieTitle.AutoSize = true;
-            lblMovieTitle.Location = new Point(109, 62);
+            lblMovieTitle.Location = new Point(109, 34);
             lblMovieTitle.Name = "lblMovieTitle";
             lblMovieTitle.Size = new Size(65, 15);
             lblMovieTitle.TabIndex = 0;
-            lblMovieTitle.Text = "Movie Title";
+            lblMovieTitle.Text = "Movie &Title";
             // 
             // txtMovieTitle
             // 
-            txtMovieTitle.Location = new Point(184, 59);
+            txtMovieTitle.Location = new Point(184, 31);
             txtMovieTitle.Name = "txtMovieTitle";
             txtMovieTitle.Size = new Size(226, 23);
             txtMovieTitle.TabIndex = 1;
@@ -58,35 +60,35 @@
             // lblMoviePrice
             // 
             lblMoviePrice.AutoSize = true;
-            lblMoviePrice.Location = new Point(109, 95);
+            lblMoviePrice.Location = new Point(141, 81);
             lblMoviePrice.Name = "lblMoviePrice";
-            lblMoviePrice.Size = new Size(69, 15);
+            lblMoviePrice.Size = new Size(33, 15);
             lblMoviePrice.TabIndex = 2;
-            lblMoviePrice.Text = "Movie Price";
+            lblMoviePrice.Text = "&Price";
             // 
             // lblMovieGenre
             // 
             lblMovieGenre.AutoSize = true;
-            lblMovieGenre.Location = new Point(109, 129);
+            lblMovieGenre.Location = new Point(136, 127);
             lblMovieGenre.Name = "lblMovieGenre";
-            lblMovieGenre.Size = new Size(74, 15);
+            lblMovieGenre.Size = new Size(38, 15);
             lblMovieGenre.TabIndex = 4;
-            lblMovieGenre.Text = "Movie Genre";
+            lblMovieGenre.Text = "&Genre";
             // 
             // numMoviePrice
             // 
             numMoviePrice.DecimalPlaces = 2;
-            numMoviePrice.Location = new Point(184, 93);
+            numMoviePrice.Location = new Point(184, 79);
             numMoviePrice.Name = "numMoviePrice";
             numMoviePrice.Size = new Size(122, 23);
-            numMoviePrice.TabIndex = 6;
+            numMoviePrice.TabIndex = 3;
             // 
             // btnAddMovie
             // 
             btnAddMovie.Location = new Point(109, 297);
             btnAddMovie.Name = "btnAddMovie";
             btnAddMovie.Size = new Size(75, 23);
-            btnAddMovie.TabIndex = 7;
+            btnAddMovie.TabIndex = 8;
             btnAddMovie.Text = "&Add Movie";
             btnAddMovie.UseVisualStyleBackColor = true;
             btnAddMovie.Click += btnAddMovie_Click;
@@ -94,10 +96,11 @@
             // cboMovieGenre
             // 
             cboMovieGenre.FormattingEnabled = true;
-            cboMovieGenre.Location = new Point(184, 126);
+            cboMovieGenre.Location = new Point(184, 124);
             cboMovieGenre.Name = "cboMovieGenre";
             cboMovieGenre.Size = new Size(146, 23);
-            cboMovieGenre.TabIndex = 8;
+            cboMovieGenre.Sorted = true;
+            cboMovieGenre.TabIndex = 5;
             cboMovieGenre.TextChanged += cboMovieGenre_TextChanged;
             // 
             // lstMovieOutput
@@ -107,13 +110,36 @@
             lstMovieOutput.Location = new Point(109, 169);
             lstMovieOutput.Name = "lstMovieOutput";
             lstMovieOutput.Size = new Size(301, 109);
-            lstMovieOutput.TabIndex = 9;
+            lstMovieOutput.TabIndex = 6;
+            lstMovieOutput.TabStop = false;
+            // 
+            // btnClear
+            // 
+            btnClear.Location = new Point(221, 297);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(75, 23);
+            btnClear.TabIndex = 9;
+            btnClear.Text = "&Clear";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnQuit
+            // 
+            btnQuit.Location = new Point(335, 297);
+            btnQuit.Name = "btnQuit";
+            btnQuit.Size = new Size(75, 23);
+            btnQuit.TabIndex = 10;
+            btnQuit.Text = "&Quit";
+            btnQuit.UseVisualStyleBackColor = true;
+            btnQuit.Click += btnQuit_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(518, 378);
+            Controls.Add(btnQuit);
+            Controls.Add(btnClear);
             Controls.Add(lstMovieOutput);
             Controls.Add(cboMovieGenre);
             Controls.Add(btnAddMovie);
@@ -140,5 +166,7 @@
         private Button btnAddMovie;
         private ComboBox cboMovieGenre;
         private ListBox lstMovieOutput;
+        private Button btnClear;
+        private Button btnQuit;
     }
 }
--- MovieSalesT/Form1.cs
+++ MovieSalesT/Form1.cs
@@ -7,13 +7,18 @@ namespace MovieSalesT
             InitializeComponent();
         }
 
+        string genreFile = "MovieGenre.txt";
+        StreamReader sr;
+        StreamWriter sw; // Corrected type from StreamReader to StreamWriter
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            cboMovieGenre.Items.Add("Action");
-            cboMovieGenre.Items.Add("Comedy");
-            cboMovieGenre.Items.Add("Horror");
-            cboMovieGenre.Items.Add("Sci-Fi");
-            cboMovieGenre.Items.Add("Romance");
+            sr = File.OpenText(genreFile);
+            while (!sr.EndOfStream)
+            {
+                cboMovieGenre.Items.Add(sr.ReadLine().Trim());
+            }
+            sr.Close();
         }
 
         private void cboMovieGenre_TextChanged(object sender, EventArgs e)
@@ -23,8 +28,28 @@ namespace MovieSalesT
 
         private void btnAddMovie_Click(object sender, EventArgs e)
         {
-            Movie movie = new Movie(txtMovieTitle.Text, (double) numMoviePrice.Value, cboMovieGenre.Text);
+            Movie movie = new Movie(txtMovieTitle.Text, (double)numMoviePrice.Value, cboMovieGenre.Text);
             lstMovieOutput.Items.Add(movie);
+            //add any new genre to combo box
+            if (!cboMovieGenre.Items.Contains(movie.MovieGenre))
+            {
+                cboMovieGenre.Items.Add(movie.MovieGenre);
+                sw = File.AppendText(genreFile);
+                sw.WriteLine(movie.MovieGenre);
+                sw.Close();
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtMovieTitle.Clear();
+            cboMovieGenre.Text = "";
+            numMoviePrice.Value = numMoviePrice.Minimum;
         }
     }
 }
